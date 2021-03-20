53935430d1ee1fe1965ea89cbc634df07cdce5a6
Mei Chu <thiag0mei@icloud.com>
2021-03-20 17:56:50 -0300

    Update MrVault

--- MrVault.py
+++ MrVault.py
@@ -1,14 +1,15 @@
 import discord
 from discord.ext import commands
-import yugioh
-import Flip
+#import yugioh
+#import Flip
 import russianroulette
-
+import genshin
 bot = commands.Bot(command_prefix='%')
 
-bot.add_command(yugioh.rand)
-bot.add_command(Flip.flip)
-bot.add_command(yugioh.card)
+#bot.add_command(yugioh.rand)
+#bot.add_command(Flip.flip)
+#bot.add_command(yugioh.card)
 bot.add_command(russianroulette.rr)
-
+bot.add_command(genshin.genshin_time)
+bot.add_cog(genshin.EditCog(bot))
 bot.run('NzM3ODQyNzA4MjgxOTUwMjk5.XyDPkg.9bS0EMpn9Y9gerpPJnSddRfrWCQ')
